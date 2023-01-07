74ec3d972329ae3d5239cf0877dbd25e5a6f04f9
Xavier Serrano <xavier.serrano.rico@hp.com>
2023-01-07 19:26:29 +0100

    feat: add timer

--- src/components/Pages/Page1.tsx
+++ src/components/Pages/Page1.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Image4 from "../../images/prueba.png";
+import { Timer } from "./Timer";
 
 const ImgDiv = styled.div`
 height:100%
@@ -11,7 +12,7 @@ export const Page1: React.FC = () => {
     //  <label></label>
     <ImgDiv>
    <img src={Image4} height= "100%"></img>
-   <label >Prueba</label>
+   <Timer deadline="June, 3, 2023, 13:00"></Timer>
    </ImgDiv>
   );
 };
