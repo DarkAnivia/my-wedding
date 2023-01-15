21c66f986dfa37f5b2dd2f487adb25278037c7d7
Xavier Serrano <xavier.serrano.rico@hp.com>
2023-01-15 14:30:01 +0100

    feat: timer

--- src/components/Pages/Page4.tsx
+++ src/components/Pages/Page4.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Page3Wrapper, Page2Container } from "./styled";
-import { Text } from "../Text";
+import Sonic from "../../images/sonic.png";
+import Mega from "../../images/mega.png";
+import Mario from "../../images/mario.png";
+import Kidd from "../../images/kidd.png";
 import { Timer } from "../Timer/Timer";
+import { Divider } from "../Divider";
 
 
 export const Page4: React.FC = () => {
@@ -11,7 +15,11 @@ export const Page4: React.FC = () => {
         {/* <Text fontSize="18px" paddingBottom="18px">
           Placeholder.
         </Text> */}
-        <Timer deadline="June, 3, 2023, 13:00"></Timer>
+
+          <div className='timer-up'>    <img src={Sonic} className='timer-image-l'></img>    <img src={Mega} className='timer-image-r'></img></div>
+          <Timer deadline="June, 3, 2023, 13:00"></Timer>
+          <div className='timer-down'>    <img src={Mario} className='timer-image-l'></img>    <img src={Kidd} className='timer-image-r'></img></div>
+          <Divider></Divider>
 
       </Page2Container>
     </Page3Wrapper>
